52686032275cf2f04702657cd120f58c98015704
memersfriend <121755302+memersfriend@users.noreply.github.com>
2024-03-03 10:51:51 -0500

    initial commit

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  }
+}
+
+module.exports = nextConfig;
